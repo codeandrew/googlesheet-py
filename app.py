5fd3c2a578454acb396f7fcc891fc1b165855404
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2021-02-02 13:29:23 +0800

    Finished Shopify API

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import gspread
 from flask import Flask, jsonify, request, abort
 from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
+
 # Google Sheets API Setup
 
 credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",
@@ -37,20 +38,22 @@ def del_review(email):
 
 @app.route('/add_row', methods=["POST"])
 def add_row():
-    req = request.get_json()
-    print(req)
-    row = [
-        req["transaction_id"],
-        req["item_id"],
-        req["item_name"],
-        req["name"],
-        req["number"],
-        req["size"],
-        req["date"]
-         ]
-    gsheet.insert_row(row, 2)  # since the first row is our title header
-    return jsonify(gsheet.get_all_records())
+    postreq = request.get_json()
 
+    for req in postreq:
+
+        row = [
+            req["transaction_id"],
+            req["item_id"],
+            req["item_name"],
+            req["name"],
+            req["number"],
+            req["size"],
+            req["date"]
+            ]
+        gsheet.insert_row(row, 2)  # since the first row is our title header
+
+    return jsonify(gsheet.get_all_records())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 8080))
--- curl-json.sh
+++ curl-json.sh
@@ -2,10 +2,13 @@
 set -x
 
 COUNTER=$1
-SUBPATH="add_review"
-URL="http://0.0.0.0:32772/${SUBPATH}"
-JSON='{"email":"example@email.com","date": "2021-01-01" ,"score":"$COUNTER" }'
+SUBPATH="add_row"
+URL="http://0.0.0.0:8080/${SUBPATH}"
+#JSON='{"email":"example@email.com","date": "2021-01-01" ,"score":"$COUNTER" }'
 
+# JSON='{ "transaction_id" : 123123456456, "item_id": 123, "item_name": "Basic T-shirt", "name": "Billy Santors","number": 10,"size": "XXL", "date": "2021-01-01" }'
+
+JSON='[{ "transaction_id" : 100000000001, "item_id": 7, "item_name": "Basic T-shirt", "name": "Mia Khalifa","number": 6,"size": "M", "date": "2021-01-01" }, { "transaction_id" : 100000000001, "item_id": 7, "item_name": "Basic T-shirt", "name": "Eimi Fukada","number": 9,"size": "XS", "date": "2021-01-01" }, { "transaction_id" : 100000000001, "item_id": 7, "item_name": "Basic T-shirt", "name": "Lexi Lore","number": 12,"size": "XXS", "date": "2021-01-01" }]'
 echo $URL
 curl -d "$JSON" -H 'Content-Type: application/json' $URL
 
